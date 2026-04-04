ebefd6ed938c7fd30ff782c1f00534b9e94c52ba
farruxxudarganov <farruxxudarganov1@gmail.com>
2026-04-04 16:28:06 +0500

    Python Lessons

--- return-function.py
+++ return-function.py
@@ -11,11 +11,35 @@
 # print(add_numbers(8, 7)) # 15
 # result = add_numbers(4, 6) # 10
 # print(result) # 10
+# def isEven(number):
+#     if number % 2 == 0:
+#         return "Juft"
+#     else:
+#         return "Toq"
+# print(isEven(4)) # Juft
+# print(isEven(7)) # Toq
 
-def juft_toq(son):
-    if son % 2 == 0:
-        return f"{son} - juft son"
-    else:
-        return f"{son} - toq son"
-son = int(input("Son kiriting: "))
-print(juft_toq(son))
+
+
+# Ternary operator - shartli ifoda, qisqa if-else yozish usuli
+# def isEven(number):
+#     return "juft" if number % 2 == 0 else "toq"
+# print(isEven(4)) # juft
+# print(isEven(7)) # toq
+
+
+
+# def toliq_ism_yasa(ism, familiya):
+#     toliq_ism = f"{ism} {familiya}"
+#     return toliq_ism
+
+
+# talaba1 = toliq_ism_yasa("olim", "hakimov")
+# talaba2 = toliq_ism_yasa("hakim", "olimov")
+# print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
+# print(f"{talaba1} darsga kechikib keldi")
+
+
+age= int(input("Yoshingizni kiriting: "))
+
+print("Siz endi kattasiz") if age >= 18 else print("Siz hali yoshsiz")
---
+++ string-loop.py
@@ -0,0 +1,39 @@
+text = "Python Lessons"
+# print(len(text)) # 14
+# string ichidagi har bir belgi indeksga ega (0 dan boshlanadi)
+# print(text[0]) # P
+# print(text[1]) # y
+# print(text[2]) # t
+# print(text[6]) #  
+# print(text[13]) # s
+# print(text[-1]) # s
+# print(text[-2]) # n
+# string ichidagi oxirgi belgi indeks olish uchun -1 dan foydalaniladi
+# print(text[len(text)-1]) # s
+# for tsikl
+# 1-usul
+# for belgi in text:
+#     print(belgi)
+# # 2-usul
+# for index in range(len(text)):
+#     print(index , text[index])
+
+
+txt = "Bugun darsga Xursandbek kelmadi"
+#  in operatori - string ichida ma'lum bir belgining bor yoki yo'qligini tekshirish uchun ishlatiladi
+print("Xursandbek" in txt) # True
+print("Asadbek" in txt) # False
+print("y" in txt) # False
+print("a" in txt) # True
+
+# unli haflar (a, e, i, o, u)
+# Example "Salom" -> 2 unli harf(a, o)
+
+vowels = "aeiou"
+def count_vowels(word):
+    count= 0
+    for letter in word:
+        if letter in vowels:
+            count += 1
+    return count
+print(count_vowels("Salom")) 
