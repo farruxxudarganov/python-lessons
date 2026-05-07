5495868b928ac97bcf27b57c35b3703e05626f48
farruxxudarganov <farruxxudarganov1@gmail.com>
2026-05-07 15:13:06 +0500

    lessons in python

--- lambda-function.py
+++ lambda-function.py
@@ -26,8 +26,39 @@
 # map(sqrt , sonlar)
 # ildizlar = list(map(sqrt, sonlar))
 # print(ildizlar)
-numbers = list(map(int, input().split()))
-print(numbers)
+# numbers = list(map(int, input().split()))
+# print(numbers)
 #  0 1 5 8 12 -5
 # print("Hello World".split())
 # print("0 1 5 8 12 -5".split())
+sonlar = list(range(11)) # 0 dan 10 gacha sonlar ro'yxati
+
+# def daraja2(x):
+#     """Berilgan sonning kvadratini qaytaruvchi funksiya"""
+#     return x*x
+
+# print(list(map(daraja2,sonlar))) # sonlar ning kvadratini hisoblaymiz
+
+# kvadratlar = list(map(lambda x: x*x, sonlar))
+# print(kvadratlar)
+
+# kvadratlar = []
+# for son in sonlar:
+#     kvadratlar.append(son*son)
+
+# print(kvadratlar)
+
+a = [4, 5, 6]
+b = [7, 8, 9]
+a_plus_b = list(map(lambda x,y:x+y,a,b))
+print(a_plus_b)
+
+numbers = [1,2 ,3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x%2 != 0, numbers))
+print(odd_numbers)
+print(list(filter(lambda x: x > 5, numbers)))
+
+mevalar = ['olma','anor','anjir','shaftoli',"o'rik","tarvuz","qovun","banan"]
+
+mevalar_b = list(filter(lambda meva:meva.startswith('b'),mevalar))
+print(mevalar_b)
